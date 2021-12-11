9c00a4896f018f847c613b0ec8d1fba8b221b8a4
Andrew Horvath <ajhorvath@pm.me>
2021-12-10 21:55:12 -0500

    add top level imports

--- motive/__init__.py
+++ motive/__init__.py
@@ -1,1 +1,9 @@
+from motive.context import Context
+from motive.exceptions import RunnerError, IteratorError
+from motive.properties import Properties
+from motive.nest import nest
+from motive.run import run
+
+
 __version__ = "0.1.0"
+
